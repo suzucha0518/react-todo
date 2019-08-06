dd5be4a21d66a5a8bc86dcba1e3479cecdc74793
suzucha0518 <suzucha0518@gmail.com>
2019-08-06 21:03:55 +0900

    Updates

---
+++ precache-manifest.7637b7fbf49ee5057e7e52b3c0ebbbcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb30a336dbff6fd6d60db6776a78d13a",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "fcd00f5a0a5259226116",
+    "url": "/react-todo/static/css/main.6a5a5328.chunk.css"
+  },
+  {
+    "revision": "caa9a7fa08bf2c0d73b8",
+    "url": "/react-todo/static/js/2.f0910273.chunk.js"
+  },
+  {
+    "revision": "fcd00f5a0a5259226116",
+    "url": "/react-todo/static/js/main.47926842.chunk.js"
+  },
+  {
+    "revision": "052fad23ca9f036fcf47",
+    "url": "/react-todo/static/js/runtime~main.79666361.js"
+  },
+  {
+    "revision": "4a9b654fa58cb809461bf4d21413844a",
+    "url": "/react-todo/static/media/profile.4a9b654f.png"
+  }
+]);
